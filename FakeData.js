abe6ff8868a07c883a09d9cead2405f24fe6b514
Mylene <pont.mylene@gmail.com>
2022-02-07 13:31:06 +0900

    module 3 finished, fakedata added, first call to Apollo

---
+++ FakeData.js
@@ -0,0 +1,46 @@
+const UserList = [
+    {
+        id: 1,
+        name: "John",
+        username: "john",
+        age: 20,
+        nationality: "Canada",
+    },
+    {
+        id: 2,
+        name: "Pedro",
+        username: "PedroTech",
+        age: 20,
+        nationality: "Brazil",
+    },
+    {
+        id: 3,
+        name: "Sarah",
+        username: "cameron",
+        age: 25,
+        nationality: "United States",
+    },
+    {
+        id: 4,
+        name: "Marc",
+        username: "Marco",
+        age: 43,
+        nationality: "France",
+    },
+    {
+        id: 5,
+        name: "Kiko",
+        username: "Kiki-chan",
+        age: 15,
+        nationality: "Japan",
+    },
+    {
+        id: 6,
+        name: "Jayoong",
+        username: "Jay",
+        age: 37,
+        nationality: "South Korea",
+    },
+];
+
+module.exports = {UserList}
