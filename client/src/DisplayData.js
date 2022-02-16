14be4f57b6638e144f2b42768ff9f754947288ec
Mylene <pont.mylene@gmail.com>
2022-02-16 16:54:27 +0100

    create new user done, refetch

--- client/src/DisplayData.js
+++ client/src/DisplayData.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { useQuery, gql, useLazyQuery } from '@apollo/client';
+import { useQuery, gql, useLazyQuery, useMutation } from '@apollo/client';
 
 const mystyle = {
     color: "white",
@@ -55,21 +55,39 @@ const GET_MOVIE_BY_NAME = gql`
     }
 `;
 
+const CREATE_USER_MUTATION = gql`
+    mutation CreateUser($input: CreateUserInput!) {
+        createUser(input: $input) {
+            name
+            id
+        }
+    }
+`
+
 const DisplayData = () => {
+    // Movies states
+    const {data: movieData } = useQuery(QUERY_ALL_MOVIES);
+
     const [movieSearched, setMovieSearched] = useState("");
     const [fetchMovie, {data: movieSearchedData, error: movieError}] = useLazyQuery(GET_MOVIE_BY_NAME);
 
-    const {data, loading, error} = useQuery(QUERY_ALL_USERS);
-    const {data: movieData } = useQuery(QUERY_ALL_MOVIES);
+    // User states
+    const {data, loading, refetch} = useQuery(QUERY_ALL_USERS);
 
+    const [name, setName] = useState("");
+    const [username, setUsername] = useState("");
+    const [age, setAge] = useState(0);
+    const [nationality, setNationality] = useState("");
+    
+    const [createUser] = useMutation(CREATE_USER_MUTATION)
 
     if(loading){
         return <h1>Data loading</h1>
     }
 
-    if(error){
-        console.log(error)
-    }
+    // if(error){
+    //     console.log(error)
+    // }
 
     if(movieError){
         console.log(movieError)
@@ -77,6 +95,24 @@ const DisplayData = () => {
 
     return (
         <div>
+            <div>
+                <input type="text" placeholder='Name...' onChange={(event) => {
+                    setName(event.target.value);}} />
+                <input type="text" placeholder='Username...' onChange={(event) => {
+                    setUsername(event.target.value);}} />
+                <input type="number" placeholder='Age...' onChange={(event) => {
+                    setAge(event.target.value);}} />
+                <input type="text" placeholder='Nationality...'onChange={(event) => {
+                    setNationality(event.target.value.toUpperCase());}}  />
+
+                <button
+                    onClick={() => {
+                        createUser({ variables: { input: {name, username, age: Number(age), nationality}}});
+                        refetch()
+                    }}
+                >Create User</button>
+            </div>
+            
             <h1 style={mystyle}>USERS</h1>
             {data && 
                 data.users.map((user) => {
@@ -84,6 +120,7 @@ const DisplayData = () => {
                         <div style={card}>
                             <h1>Name: {user.name}</h1>
                             <h1>Username: {user.username}</h1>
+                            <h1>Nationality: {user.nationality}</h1>
                             <h1>Age: {user.age}</h1>
                         </div>
                     )
@@ -129,3 +166,6 @@ const DisplayData = () => {
 };
 
 export default DisplayData;
+
+
+// module 8 3:18
